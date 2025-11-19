13deffd8c4906514767058bf7c3d9b73a982e5fb
Byuntil <gugugu2014@jbnu.ac.kr>
2025-11-20 00:09:53 +0900

    first commit

---
+++ src/main/java/eusyaeusya/gmg/GmgApplication.java
@@ -0,0 +1,13 @@
+package eusyaeusya.gmg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/eusyaeusya/gmg/GmgApplicationTests.java
@@ -0,0 +1,13 @@
+package eusyaeusya.gmg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
