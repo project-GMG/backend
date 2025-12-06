dde67bec90505cfcf776c7809195a061563abdc4
Byuntil <gugugu2014@jbnu.ac.kr>
2025-12-06 23:37:42 +0900

    fix: place_type insert 스크립트 수정

--- src/main/resources/db/migration/V2__insert_initial_place_types.sql
+++ src/main/resources/db/migration/V2__insert_initial_place_types.sql
@@ -1,6 +1,6 @@
 -- 장소 타입 초기 데이터 삽입
-INSERT INTO place_types (code, label, created_at)
-VALUES ('RESTAURANT', '식당', CURRENT_TIMESTAMP),
-       ('CAFE', '카페', CURRENT_TIMESTAMP),
-       ('BAR', '술집', CURRENT_TIMESTAMP),
-       ('STUDY', '스터디', CURRENT_TIMESTAMP);
+INSERT INTO place_types (code, label)
+VALUES ('RESTAURANT', '식당'),
+       ('CAFE', '카페'),
+       ('BAR', '술집'),
+       ('STUDY', '스터디');
