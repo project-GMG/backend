5c48b956007d9bee77b0fc4e3a0fcb8a5affda7a
Byuntil <gugugu2014@jbnu.ac.kr>
2025-12-08 02:34:16 +0900

    fix: flyway 구문 오류 수정
    local에서 ./gradlew flywayMigrate 로 미리 테스트 할 수 있도록 수정 + h2, mysql 문법 충돌 문제 수정

--- src/main/resources/db/migration/V1__create_place_types_table.sql
+++ src/main/resources/db/migration/V1__create_place_types_table.sql
@@ -4,6 +4,7 @@ CREATE TABLE place_types
     code  VARCHAR(32) NOT NULL,
     label VARCHAR(32) NOT NULL,
     CONSTRAINT pk_place_types PRIMARY KEY (id),
-    CONSTRAINT uc_place_types_code UNIQUE (code),
-    INDEX idx_code (code)
-) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+    CONSTRAINT uc_place_types_code UNIQUE (code)
+);
+
+CREATE INDEX idx_code ON place_types(code);
