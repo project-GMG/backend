35f306c3e15befebe6a753809aa94c00d5a888cc
Byuntil <gugugu2014@jbnu.ac.kr>
2025-12-08 00:19:23 +0900

    feat(EventErrorCode): 이벤트 에러코드 추가

---
+++ src/main/java/eusyaeusya/gmg/api/event/response/EventErrorCode.java
@@ -0,0 +1,21 @@
+package eusyaeusya.gmg.api.event.response;
+
+import eusyaeusya.gmg.common.api.response.code.ErrorCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor(access = lombok.AccessLevel.PACKAGE)
+@Getter
+public enum EventErrorCode implements ErrorCode {
+    // 조회 관련 (EV-0001 ~ EV-0009)
+
+    // 상태 관련 (EV-0100 ~ EV-0199)
+
+    // 비즈니스 규칙 위반(EV-0200 ~ EV-0299)
+    INVALID_DATE_RANGE("EV-0200", "시작 날짜는 종료 날짜보다 이전이어야 합니다"),
+    INVALID_TIME_RANGE("EV-0201", "시작 시간은 종료 시간보다 이전이어야 합니다"),
+    DATE_RANGE_TOO_LONG("EV-0202", "날짜 범위는 최대 31일을 초과할 수 없습니다");
+
+    private final String value;
+    private final String message;
+}
