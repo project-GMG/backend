2a30f35fae0e584a9f16922e5a1cebf16856b9c5
Byuntil <gugugu2014@jbnu.ac.kr>
2025-12-09 21:12:33 +0900

    feat(ParticipantUnavailableTime): 불가능한 시간 엔티티, 리포지토리 추가

---
+++ src/main/java/eusyaeusya/gmg/domain/participant/repository/ParticipantUnavailableTime.java
@@ -0,0 +1,6 @@
+package eusyaeusya.gmg.domain.participant.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParticipantUnavailableTime extends JpaRepository<ParticipantUnavailableTime, Long> {
+}
