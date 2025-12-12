3ddb1bf584605e0d608d98035804ee9647ca6c90
Byuntil <gugugu2014@jbnu.ac.kr>
2025-12-12 17:56:55 +0900

    feat(PlaceController): 장소 조회 api 구현

--- src/main/java/eusyaeusya/gmg/api/place/response/PlaceErrorCode.java
+++ src/main/java/eusyaeusya/gmg/api/place/response/PlaceErrorCode.java
@@ -9,7 +9,11 @@ import lombok.RequiredArgsConstructor;
 public enum PlaceErrorCode implements ErrorCode {
     //조회 관련 (PL-E0001 ~ PL-E0009)
     PLACE_TYPE_NOT_FOUND("PL-E0001", "장소 타입을 찾을 수 없습니다"),
-    ;
+    PLACE_CATEGORY_NOT_FOUND("PL-E0002", "장소 카테고리를 찾을 수 없습니다"),
+
+    //상태 관련 (PL-E0100 ~ PL-E0199)
+    CATEGORY_NOT_IN_EVENT_PLACE_TYPES("PL-E0100", "장소 카테고리는 이벤트 장소 타입에 속하지 않습니다");
+
     private final String value;
     private final String message;
 }
