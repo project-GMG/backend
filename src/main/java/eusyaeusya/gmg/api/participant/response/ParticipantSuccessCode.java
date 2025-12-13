5fffcd4e385359dcdf1b79c4e055f0575918bef4
Byuntil <gugugu2014@jbnu.ac.kr>
2025-12-13 16:18:02 +0900

    feat(ParticipantController): 참여자 최종 등록 api 구현

--- src/main/java/eusyaeusya/gmg/api/participant/response/ParticipantSuccessCode.java
+++ src/main/java/eusyaeusya/gmg/api/participant/response/ParticipantSuccessCode.java
@@ -10,6 +10,7 @@ public enum ParticipantSuccessCode implements SuccessCode {
     PARTICIPANT_JOINED("PT-S0001", "참여자 등록이 완료되었습니다"),
     UNAVAILABLE_TIME_REGISTERED("PT-S0002", "불가능한 시간 등록이 완료되었습니다"),
     DISLIKED_REGISTERED("PT-S0003", "비선호 카테고리 및 장소 등록이 완료되었습니다"),
+    PARTICIPANT_COMPLETED("PT-S0004", "참여자 정보 입력이 완료되었습니다"),
     ;
     private final String value;
     private final String message;
