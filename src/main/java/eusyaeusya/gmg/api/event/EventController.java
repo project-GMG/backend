633fea782d426d06f466432f594dc7aa3847bf03
Byuntil <gugugu2014@jbnu.ac.kr>
2025-12-19 20:34:52 +0900

    feat(EventController): 히트맵 데이터 포함된 이벤트 정보 조회 api 구현

--- src/main/java/eusyaeusya/gmg/api/event/EventController.java
+++ src/main/java/eusyaeusya/gmg/api/event/EventController.java
@@ -2,15 +2,19 @@ package eusyaeusya.gmg.api.event;
 
 import eusyaeusya.gmg.api.event.request.EventCreateRequest;
 import eusyaeusya.gmg.api.event.response.EventCreateResponse;
+import eusyaeusya.gmg.api.event.response.EventMainResponse;
 import eusyaeusya.gmg.api.event.response.EventPlaceTypeCategoriesResponse;
 import eusyaeusya.gmg.api.event.response.EventSuccessCode;
 import eusyaeusya.gmg.common.api.response.ApiResponse;
+import eusyaeusya.gmg.config.sse.SseService;
 import eusyaeusya.gmg.domain.event.service.EventPlaceTypeService;
 import eusyaeusya.gmg.domain.event.service.EventService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Slf4j
 @RestController
@@ -20,6 +24,7 @@ public class EventController implements EventApiSpec {
 
     private final EventService eventService;
     private final EventPlaceTypeService eventPlaceTypeService;
+    private final SseService sseService;
 
     @Override
     @PostMapping
@@ -43,5 +48,22 @@ public class EventController implements EventApiSpec {
                 EventSuccessCode.EVENT_PLACE_TYPES_CATEGORIES_RETRIEVED,
                 response
         );
+    }
+
+    @Override
+    @GetMapping("/{hashUrl}")
+    public ApiResponse<EventMainResponse> getEventMain(@PathVariable String hashUrl) {
+        log.info("GET /events/{} - 이벤트 메인 페이지 정보 조회", hashUrl);
+        EventMainResponse response = eventService.getEventMain(hashUrl);
+
+        return ApiResponse.successWithData(EventSuccessCode.EVENT_MAIN_RETRIEVED, response);
+    }
+
+
+    @GetMapping(value = "/{hashUrl}/heatmap/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter streamHeatmap(@PathVariable String hashUrl) {
+        log.info("GET /events/{}/heatmap/stream - SSE 구독 시작", hashUrl);
+
+        return sseService.subscribe(hashUrl);
     }
 }
--- src/main/java/eusyaeusya/gmg/api/event/response/EventSuccessCode.java
+++ src/main/java/eusyaeusya/gmg/api/event/response/EventSuccessCode.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 public enum EventSuccessCode implements SuccessCode {
     EVENT_RETRIEVED("EV-S0001", "이벤트 생성 성공했습니다."),
     EVENT_PLACE_TYPES_CATEGORIES_RETRIEVED("EV-S0002", "장소 카테고리 조회 성공했습니다"),
+    EVENT_MAIN_RETRIEVED("EV-S0003", "메인 페이지 정보 조회 성공했습니다"),
     ;
 
     private final String value;
