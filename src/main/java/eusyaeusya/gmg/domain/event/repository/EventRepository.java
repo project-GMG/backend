20d2193ff652849be71e81757510bcd1aa5a2d66
Byuntil <gugugu2014@jbnu.ac.kr>
2025-12-19 20:28:42 +0900

    feat(EventRepository): 불가능한 시간 등록시 동시성 제어를 위해 쓰기락 추가

--- src/main/java/eusyaeusya/gmg/domain/event/repository/EventRepository.java
+++ src/main/java/eusyaeusya/gmg/domain/event/repository/EventRepository.java
@@ -1,10 +1,18 @@
 package eusyaeusya.gmg.domain.event.repository;
 
 import eusyaeusya.gmg.domain.event.entity.Event;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
     Optional<Event> findByHashUrl(String hashUrl);
+    
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT e FROM Event e WHERE e.hashUrl =:hashUrl")
+    Optional<Event> findByHashUrlWithLock(@Param("hashUrl") String hashUrl);
 }
