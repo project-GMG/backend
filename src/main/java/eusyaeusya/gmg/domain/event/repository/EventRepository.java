e7927aa9eac9f1b0bd6e76f5b15e9b4dc9f14749
Byuntil <gugugu2014@jbnu.ac.kr>
2026-01-09 16:56:49 +0900

    feat(EventRepository): 한계일자보다 이전에 만들어지고 만료처리되지 않은 이벤트 조회

--- src/main/java/eusyaeusya/gmg/domain/event/repository/EventRepository.java
+++ src/main/java/eusyaeusya/gmg/domain/event/repository/EventRepository.java
@@ -7,12 +7,17 @@ import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
     Optional<Event> findByHashUrl(String hashUrl);
-    
+
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     @Query("SELECT e FROM Event e WHERE e.hashUrl =:hashUrl")
     Optional<Event> findByHashUrlWithLock(@Param("hashUrl") String hashUrl);
+
+    @Query("SELECT e FROM Event e WHERE e.status != 'EXPIRED' AND e.createdAt < :threshold")
+    List<Event> findByStatusNotExpiredAndCreatedAtBefore(@Param("threshold") LocalDateTime threshold);
 }
