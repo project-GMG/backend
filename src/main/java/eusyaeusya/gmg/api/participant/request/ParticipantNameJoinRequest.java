d28d69291c0b2dd21db4e287d5a32f6005476519
zzl-hyun <kimgihyun877@gmail.com>
2026-02-06 18:47:22 +0900

    ParticipantNameJoinRequest.java 참여자 이름 등록 예시 개선

--- src/main/java/eusyaeusya/gmg/api/participant/request/ParticipantNameJoinRequest.java
+++ src/main/java/eusyaeusya/gmg/api/participant/request/ParticipantNameJoinRequest.java
@@ -1,9 +1,11 @@
 package eusyaeusya.gmg.api.participant.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record ParticipantNameJoinRequest(
+        @Schema(description = "이름", example = "구성재", type = "string", required = true)
         @NotBlank(message = "참여자 이름은 필수입니다")
         @Size(max = 50, message = "참여자 이름은 최대 50자 입력 가능합니다")
         String name
