a6456ae9a0da926dffeee466738c940630742c5e
zzl-hyun <kimgihyun877@gmail.com>
2026-03-20 22:12:45 +0900

    fix(event): 모임 메인 참여자 수를 COMPLETED 기준으로 집계
    - getEventMain의 participantCount 계산을 완료 참여자 기준으로 변경
    - draft 참여자가 참여자 수에 반영되던 문제 해결
    - 관련 테스트를 status 기반 카운트 기준으로 정렬

--- src/test/java/eusyaeusya/gmg/domain/event/service/EventServiceTest.java
+++ src/test/java/eusyaeusya/gmg/domain/event/service/EventServiceTest.java
@@ -11,6 +11,7 @@ import eusyaeusya.gmg.domain.event.entity.EventPlaceType;
 import eusyaeusya.gmg.domain.event.entity.EventStatus;
 import eusyaeusya.gmg.domain.event.repository.EventPlaceTypeRepository;
 import eusyaeusya.gmg.domain.event.repository.EventRepository;
+import eusyaeusya.gmg.domain.participant.entity.ParticipantStatus;
 import eusyaeusya.gmg.domain.participant.repository.ParticipantRepository;
 import eusyaeusya.gmg.domain.place.entity.PlaceType;
 import eusyaeusya.gmg.domain.place.service.PlaceTypeService;
@@ -151,7 +152,7 @@ class EventServiceTest {
                 .willReturn(Optional.of(mockEvent));
         given(eventPlaceTypeRepository.findByEventWithPlaceType(mockEvent))
                 .willReturn(mockPlaceTypes);
-        given(participantRepository.countByEventId(mockEvent.getId()))
+        given(participantRepository.countByEventIdAndStatus(mockEvent.getId(), ParticipantStatus.COMPLETED))
                 .willReturn(5);
         given(heatmapService.calculateHeatmap(mockEvent))
                 .willReturn(mockHeatmap);
@@ -199,7 +200,7 @@ class EventServiceTest {
                 .willReturn(Optional.of(mockEvent));
         given(eventPlaceTypeRepository.findByEventWithPlaceType(mockEvent))
                 .willReturn(Collections.emptyList());
-        given(participantRepository.countByEventId(mockEvent.getId()))
+        given(participantRepository.countByEventIdAndStatus(mockEvent.getId(), ParticipantStatus.COMPLETED))
                 .willReturn(0);
         given(heatmapService.calculateHeatmap(mockEvent))
                 .willReturn(Collections.emptyList());
@@ -254,7 +255,7 @@ class EventServiceTest {
                 .willReturn(Optional.of(mockEvent));
         given(eventPlaceTypeRepository.findByEventWithPlaceType(mockEvent))
                 .willReturn(Collections.emptyList());
-        given(participantRepository.countByEventId(mockEvent.getId()))
+        given(participantRepository.countByEventIdAndStatus(mockEvent.getId(), ParticipantStatus.COMPLETED))
                 .willReturn(3);
         given(heatmapService.calculateHeatmap(mockEvent))
                 .willReturn(Collections.emptyList());
